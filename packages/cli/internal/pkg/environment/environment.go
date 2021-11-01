3b80545b7dc12c4dd6844564743129b159b5a6b5
Taylor Neely <tayneely@amazon.com>
2021-11-01 15:08:56 -0700

    build: use latest build images

--- packages/cli/internal/pkg/environment/environment.go
+++ packages/cli/internal/pkg/environment/environment.go
@@ -6,13 +6,13 @@ import (
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/ecr"
 )
 
-const DefaultEcrRegistry = "555741984805"
+const DefaultEcrRegistry = "680431765560"
 const DefaultEcrRegion = "us-east-1"
 
 // TODO: Implement better tag versioning system
-const DefaultCromwellTag = "2021-10-01T21-33-26Z"
-const DefaultNextflowTag = "2021-10-01T21-33-26Z"
-const DefaultWesTag = "2021-10-01T21-33-26Z"
+const DefaultCromwellTag = "64"
+const DefaultNextflowTag = "21.04.3"
+const DefaultWesTag = "0.1.0"
 
 const WesImageKey = "WES"
 const CromwellImageKey = "CROMWELL"
@@ -22,19 +22,19 @@ var CommonImages = map[string]ecr.ImageReference{
 	WesImageKey: {
 		RegistryId:     LookUpEnvOrDefault("ECR_WES_ACCOUNT_ID", DefaultEcrRegistry),
 		Region:         LookUpEnvOrDefault("ECR_WES_REGION", DefaultEcrRegion),
-		RepositoryName: "agc-wes-adapter-cromwell",
+		RepositoryName: "aws/wes-release",
 		ImageTag:       LookUpEnvOrDefault("ECR_WES_TAG", DefaultWesTag),
 	},
 	CromwellImageKey: {
 		RegistryId:     LookUpEnvOrDefault("ECR_CROMWELL_ACCOUNT_ID", DefaultEcrRegistry),
 		Region:         LookUpEnvOrDefault("ECR_CROMWELL_REGION", DefaultEcrRegion),
-		RepositoryName: "cromwell",
+		RepositoryName: "aws/cromwell-mirror",
 		ImageTag:       LookUpEnvOrDefault("ECR_CROMWELL_TAG", DefaultCromwellTag),
 	},
 	NextflowImageKey: {
 		RegistryId:     LookUpEnvOrDefault("ECR_NEXTFLOW_ACCOUNT_ID", DefaultEcrRegistry),
 		Region:         LookUpEnvOrDefault("ECR_NEXTFLOW_REGION", DefaultEcrRegion),
-		RepositoryName: "nextflow",
+		RepositoryName: "aws/nextflow-mirror",
 		ImageTag:       LookUpEnvOrDefault("ECR_NEXTFLOW_TAG", DefaultNextflowTag),
 	},
 }
