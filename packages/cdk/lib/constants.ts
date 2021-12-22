4d00c914c5b26e46bc9c7cf94d4141ac8787e0a0
Elliot Smith <elliotsm@amazon.com>
2021-12-22 15:25:34 -0800

    Fixes execution order

--- packages/cdk/lib/constants.ts
+++ packages/cdk/lib/constants.ts
@@ -103,11 +103,11 @@ runcmd:
 
 # Setup ecs additions
 - cd /opt
-- /opt/ecs-additions/provision.sh
 - aws s3 sync \${INSTALLED_ARTIFACTS_S3_ROOT_URL}/ecs-additions/ ./ecs-additions && chmod a+x /opt/ecs-additions/provision.sh  
 - test -f ./ecs-additions/fetch_and_run.sh || sleep 5 || aws s3 sync \${INSTALLED_ARTIFACTS_S3_ROOT_URL}/ecs-additions/ ./ecs-additions && chmod a+x /opt/ecs-additions/provision.sh    
 - test -f ./ecs-additions/fetch_and_run.sh || sleep 10 || aws s3 sync \${INSTALLED_ARTIFACTS_S3_ROOT_URL}/ecs-additions/ ./ecs-additions && chmod a+x /opt/ecs-additions/provision.sh    
 - test -f ./ecs-additions/fetch_and_run.sh || shutdown -P now
+- /opt/ecs-additions/provision.sh
 
 - echo "successfully initiated"
 --==MYBOUNDARY==--
