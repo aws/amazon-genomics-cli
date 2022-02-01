e463483bf5e4f15218e2894b82173ce7afccaa49
Elliot Smith <elliotsm@amazon.com>
2022-02-01 15:33:38 -0800

    Updates the snakemake script for running Snakemake

--- packages/engines/snakemake/snakemake.aws.sh
+++ packages/engines/snakemake/snakemake.aws.sh
@@ -122,7 +122,7 @@ if [[ "$ENGINE_PROJECT" =~ ^s3://.* ]]; then
      handleManifest
     else
       ENGINE_PROJECT="${ENGINE_PROJECT_DIRECTORY}"
-      ENGINE_PARAMS="${ENGINE_PARAMS} --aws-batch-tags AWS_BATCH_PARENT_JOB_ID=${AWS_BATCH_JOB_ID}  --aws_batch_efs_project_path=/mnt/efs/snakemake/$GUID"
+      ENGINE_PARAMS="${ENGINE_PARAMS} --aws-batch-tags AWS_BATCH_PARENT_JOB_ID=${AWS_BATCH_JOB_ID}  --aws-batch-efs-project-path=snakemake/$GUID --latency-wait 30"
     fi
 fi
 echo "== Finding the project in  =="
