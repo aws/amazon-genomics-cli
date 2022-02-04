dee6bd495e46e2e6d3dbca990ef313c6e3187a98
Mark Schreiber <mrschre@amazon.com>
2022-02-04 14:54:42 -0500

    PR feedback

--- packages/cdk/lib/artifacts/batch-artifacts/ecs-additions/provision.sh
+++ packages/cdk/lib/artifacts/batch-artifacts/ecs-additions/provision.sh
@@ -76,7 +76,7 @@ function errorOrInt(){
   TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
   INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s "http://169.254.169.254/latest/meta-data/instance-id")
 
-  echo " Will attempt mark myself, $INSTANCE_ID, unhealthy so the autoscaler can start a replacement"
+  echo " Attempting to mark myself, $INSTANCE_ID, unhealthy so the autoscaler can start a replacement"
   aws autoscaling set-instance-health --instance-id "$INSTANCE_ID"  --health-status Unhealthy || echo "ERROR could not mark $INSTANCE_ID unhealthy, shutting down" && shutdown now
 }
 
