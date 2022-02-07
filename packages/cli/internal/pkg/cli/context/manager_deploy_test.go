7bd5f86a466671e07c68c17ff1f8fb57e7e9820c
a-li <dzl@88665a545034.ant.amazon.com>
2022-02-07 04:36:54 -0800

    fix: container validation with the correct region from user profile

--- packages/cli/internal/pkg/cli/context/manager_deploy_test.go
+++ packages/cli/internal/pkg/cli/context/manager_deploy_test.go
@@ -5,10 +5,12 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/cdk"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/clierror/actionableerror"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/spec"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/environment"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/logging"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,73 +29,73 @@ func TestManager_Deploy(t *testing.T) {
 		expectedProgressResultList []ProgressResult
 		contextList                []string
 	}{
-		//"deploy success": {
-		//	contextList: []string{testContextName3},
-		//	expectedProgressResultList: []ProgressResult{
-		//		{Outputs: []string{"some message"}, Context: testContextName3},
-		//	},
-		//	setupMocks: func(t *testing.T) mockClients {
-		//		mockClients := createMocks(t)
-		//		defer close(mockClients.progressStream1)
-		//		defer close(mockClients.progressStream2)
-		//		mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
-		//		mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
-		//		mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
-		//		mockClients.ssmMock.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
-		//		mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Return(testArtifactBucket, nil)
-		//		mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["NEXTFLOW"]).Return(nil)
-		//		clearContext := mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
-		//		mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName3).After(clearContext).Return(mockClients.progressStream1, nil)
-		//		displayProgressBar = mockClients.cdkMock.DisplayProgressBar
-		//		mockClients.cdkMock.EXPECT().DisplayProgressBar(fmt.Sprintf("Deploying resources for context(s) %s", []string{testContextName3}), []cdk.ProgressStream{mockClients.progressStream1}).Return([]cdk.Result{{Outputs: []string{"some message"}, ExecutionName: testContextName3}})
-		//		return mockClients
-		//	},
-		//},
-		//"multiple deploy success": {
-		//	contextList: []string{testContextName1, testContextName2},
-		//	expectedProgressResultList: []ProgressResult{
-		//		{Outputs: []string{"some message"}, Context: testContextName1},
-		//		{Outputs: []string{"some other message"}, Context: testContextName2},
-		//	},
-		//	setupMocks: func(t *testing.T) mockClients {
-		//		mockClients := createMocks(t)
-		//		defer close(mockClients.progressStream1)
-		//		defer close(mockClients.progressStream2)
-		//		mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
-		//		mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
-		//		mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
-		//		mockClients.ssmMock.EXPECT().GetOutputBucket().Times(2).Return(testOutputBucket, nil)
-		//		mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Times(2).Return(testArtifactBucket, nil)
-		//		mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["CROMWELL"]).Times(2).Return(nil)
-		//		clearContext := mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
-		//		clearContext2 := mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
-		//		mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName1).After(clearContext).Return(mockClients.progressStream1, nil)
-		//		mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName2).After(clearContext2).Return(mockClients.progressStream2, nil)
-		//		displayProgressBar = mockClients.cdkMock.DisplayProgressBar
-		//		expectedCdkResult := []cdk.Result{{Outputs: []string{"some message"}, ExecutionName: testContextName1}, {Outputs: []string{"some other message"}, ExecutionName: testContextName2}}
-		//		mockClients.cdkMock.EXPECT().DisplayProgressBar(fmt.Sprintf("Deploying resources for context(s) %s", []string{testContextName1, testContextName2}), []cdk.ProgressStream{mockClients.progressStream1, mockClients.progressStream2}).Return(expectedCdkResult)
-		//		return mockClients
-		//	},
-		//},
-		//"image does not exist": {
-		//	contextList: contextList,
-		//	expectedProgressResultList: []ProgressResult{
-		//		{Err: fmt.Errorf("some error occurred"), Context: testContextName1},
-		//	},
-		//	setupMocks: func(t *testing.T) mockClients {
-		//		mockClients := createMocks(t)
-		//		defer close(mockClients.progressStream1)
-		//		defer close(mockClients.progressStream2)
-		//		mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
-		//		mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
-		//		mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
-		//		mockClients.ssmMock.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
-		//		mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Return(testArtifactBucket, nil)
-		//		mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
-		//		mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["CROMWELL"]).Return(fmt.Errorf("some error occurred"))
-		//		return mockClients
-		//	},
-		//},
+		"deploy success": {
+			contextList: []string{testContextName3},
+			expectedProgressResultList: []ProgressResult{
+				{Outputs: []string{"some message"}, Context: testContextName3},
+			},
+			setupMocks: func(t *testing.T) mockClients {
+				mockClients := createMocks(t)
+				defer close(mockClients.progressStream1)
+				defer close(mockClients.progressStream2)
+				mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
+				mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
+				mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
+				mockClients.ssmMock.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
+				mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Return(testArtifactBucket, nil)
+				mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["NEXTFLOW"]).Return(nil)
+				clearContext := mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
+				mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName3).After(clearContext).Return(mockClients.progressStream1, nil)
+				displayProgressBar = mockClients.cdkMock.DisplayProgressBar
+				mockClients.cdkMock.EXPECT().DisplayProgressBar(fmt.Sprintf("Deploying resources for context(s) %s", []string{testContextName3}), []cdk.ProgressStream{mockClients.progressStream1}).Return([]cdk.Result{{Outputs: []string{"some message"}, ExecutionName: testContextName3}})
+				return mockClients
+			},
+		},
+		"multiple deploy success": {
+			contextList: []string{testContextName1, testContextName2},
+			expectedProgressResultList: []ProgressResult{
+				{Outputs: []string{"some message"}, Context: testContextName1},
+				{Outputs: []string{"some other message"}, Context: testContextName2},
+			},
+			setupMocks: func(t *testing.T) mockClients {
+				mockClients := createMocks(t)
+				defer close(mockClients.progressStream1)
+				defer close(mockClients.progressStream2)
+				mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
+				mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
+				mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
+				mockClients.ssmMock.EXPECT().GetOutputBucket().Times(2).Return(testOutputBucket, nil)
+				mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Times(2).Return(testArtifactBucket, nil)
+				mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["CROMWELL"]).Times(2).Return(nil)
+				clearContext := mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
+				clearContext2 := mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
+				mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName1).After(clearContext).Return(mockClients.progressStream1, nil)
+				mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName2).After(clearContext2).Return(mockClients.progressStream2, nil)
+				displayProgressBar = mockClients.cdkMock.DisplayProgressBar
+				expectedCdkResult := []cdk.Result{{Outputs: []string{"some message"}, ExecutionName: testContextName1}, {Outputs: []string{"some other message"}, ExecutionName: testContextName2}}
+				mockClients.cdkMock.EXPECT().DisplayProgressBar(fmt.Sprintf("Deploying resources for context(s) %s", []string{testContextName1, testContextName2}), []cdk.ProgressStream{mockClients.progressStream1, mockClients.progressStream2}).Return(expectedCdkResult)
+				return mockClients
+			},
+		},
+		"image does not exist": {
+			contextList: contextList,
+			expectedProgressResultList: []ProgressResult{
+				{Err: fmt.Errorf("some error occurred"), Context: testContextName1},
+			},
+			setupMocks: func(t *testing.T) mockClients {
+				mockClients := createMocks(t)
+				defer close(mockClients.progressStream1)
+				defer close(mockClients.progressStream2)
+				mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
+				mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
+				mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
+				mockClients.ssmMock.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
+				mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Return(testArtifactBucket, nil)
+				mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
+				mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["CROMWELL"]).Return(fmt.Errorf("some error occurred"))
+				return mockClients
+			},
+		},
 		"engine name does not exist": {
 			contextList: contextList,
 			expectedProgressResultList: []ProgressResult{
@@ -192,24 +194,24 @@ func TestManager_Deploy(t *testing.T) {
 				return mockClients
 			},
 		},
-		//"deploy error": {
-		//	contextList: contextList,
-		//	expectedProgressResultList: []ProgressResult{
-		//		{Err: fmt.Errorf("some context error"), Context: testContextName1},
-		//	},
-		//	setupMocks: func(t *testing.T) mockClients {
-		//		mockClients := createMocks(t)
-		//		mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
-		//		mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
-		//		mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
-		//		mockClients.ssmMock.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
-		//		mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Return(testArtifactBucket, nil)
-		//		mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
-		//		mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName1).Return(nil, fmt.Errorf("some context error"))
-		//		mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["CROMWELL"]).Return(nil)
-		//		return mockClients
-		//	},
-		//},
+		"deploy error": {
+			contextList: contextList,
+			expectedProgressResultList: []ProgressResult{
+				{Err: fmt.Errorf("some context error"), Context: testContextName1},
+			},
+			setupMocks: func(t *testing.T) mockClients {
+				mockClients := createMocks(t)
+				mockClients.configMock.EXPECT().GetUserEmailAddress().Return(testUserEmail, nil)
+				mockClients.configMock.EXPECT().GetUserId().Return(testUserId, nil)
+				mockClients.projMock.EXPECT().Read().Return(testValidProjectSpec, nil)
+				mockClients.ssmMock.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
+				mockClients.ssmMock.EXPECT().GetCommonParameter("installed-artifacts/s3-root-url").Return(testArtifactBucket, nil)
+				mockClients.cdkMock.EXPECT().ClearContext(filepath.Join(testHomeDir, ".agc/cdk/apps/context")).Return(nil)
+				mockClients.cdkMock.EXPECT().DeployApp(filepath.Join(testHomeDir, ".agc/cdk/apps/context"), gomock.Len(35), testContextName1).Return(nil, fmt.Errorf("some context error"))
+				mockClients.ecrClientMock.EXPECT().VerifyImageExists(environment.CommonImages["CROMWELL"]).Return(nil)
+				return mockClients
+			},
+		},
 		"clear context error": {
 			contextList: contextList,
 			expectedProgressResultList: []ProgressResult{
@@ -241,6 +243,7 @@ func TestManager_Deploy(t *testing.T) {
 				ecrClient: mockClients.ecrClientMock,
 				baseProps: baseProps{homeDir: testHomeDir},
 				imageRefs: environment.CommonImages,
+				region:    "us-east-1",
 			}
 
 			progressResultList := manager.Deploy(tc.contextList)
