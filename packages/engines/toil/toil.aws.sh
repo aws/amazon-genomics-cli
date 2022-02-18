cab340df7189515398c7e130bdcf290700f9534d
Adam Novak <anovak@soe.ucsc.edu>
2022-02-18 13:59:21 -0800

    Use a Toil that can generate dest buckets and mount Docker

--- packages/engines/toil/toil.aws.sh
+++ packages/engines/toil/toil.aws.sh
@@ -10,10 +10,10 @@ printenv
 
 echo "=== START SERVER ==="
 
-# We expect some AGC info in the environment: JOB_QUEUE_ARN
+# We expect some AGC info in the environment: JOB_QUEUE_ARN and ROOT_DIR
 # These come from packages/cdk/lib/env/context-app-parameters.ts
-# If we need more we'll need to add them in the Toil engine construct, or maybe stop passing getEngineContainer() down as a parameter.
-# We assume whatever role the batch jobs get when they go in the queue is the right role for them.
+# And also TOIL_AWS_BATCH_JOB_ROLE_ARN must be set in Toil's environment.
+# This comes from packages/cdk/lib/stacks/engines/toil-engine-construct.ts
 AWS_REGION=$(echo ${JOB_QUEUE_ARN} | cut -f4 -d':')
 set -x
 
@@ -23,6 +23,6 @@ export TOIL_WES_JOB_STORE_TYPE="aws"
 concurrently -n rabbitmq,celery,toil \
     "rabbitmq-server" \
     "celery --broker=${TOIL_WES_BROKER_URL} -A toil.server.celery_app worker --loglevel=INFO" \
-    "toil server --debug --host=0.0.0.0 --port=8000 --opt=--batchSystem=aws_batch '--opt=--awsBatchQueue=${JOB_QUEUE_ARN}' '--opt=--awsBatchRegion=${AWS_REGION}'"
+    "toil server --debug --host=0.0.0.0 --port=8000 --dest_bucket_base=${ROOT_DIR} --opt=--batchSystem=aws_batch '--opt=--awsBatchQueue=${JOB_QUEUE_ARN}' '--opt=--awsBatchRegion=${AWS_REGION}'"
 
 
