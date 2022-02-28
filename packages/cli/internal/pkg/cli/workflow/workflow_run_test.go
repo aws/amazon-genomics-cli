fa41fa807da8177a0d5c5055a9541ca9a37f5ac7
Adam Novak <anovak@soe.ucsc.edu>
2022-02-28 06:24:08 -0800

    feat: Explain a bit more about S3 errors (#318)

--- packages/cli/internal/pkg/cli/workflow/workflow_run_test.go
+++ packages/cli/internal/pkg/cli/workflow/workflow_run_test.go
@@ -363,6 +363,7 @@ func (s *WorkflowRunTestSuite) TestRunWorkflow_UploadToS3Failed() {
 	s.mockConfigClient.EXPECT().GetUserId().Return(testUserId, nil)
 	s.mockCfn.EXPECT().GetStackStatus(testContext1Stack).Return(types.StackStatusCreateComplete, nil)
 	errorMessage := "cannot upload to S3"
+	expectedInfix := "unable to upload s3://TestOutputBucket/project/TestProject1/userid/bender123/context/TestContext1/workflow/TestLocalWorkflowName1/workflow.zip: "
 	s.mockProjectClient.EXPECT().Read().Return(s.testProjSpec, nil)
 	s.mockProjectClient.EXPECT().GetLocation().Return(testProjectFileDir)
 	s.mockSsmClient.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
@@ -377,7 +378,7 @@ func (s *WorkflowRunTestSuite) TestRunWorkflow_UploadToS3Failed() {
 
 	actualId, err := s.manager.RunWorkflow(testContext1Name, testLocalWorkflowName, "")
 	if s.Assert().Error(err) {
-		s.Assert().EqualError(err, testErrorPrefix+errorMessage)
+		s.Assert().EqualError(err, testErrorPrefix+expectedInfix+errorMessage)
 		s.Assert().Empty(actualId)
 	}
 }
@@ -410,6 +411,7 @@ func (s *WorkflowRunTestSuite) TestRunWorkflow_UploadInputFailed() {
 	s.mockConfigClient.EXPECT().GetUserId().Return(testUserId, nil)
 	s.mockCfn.EXPECT().GetStackStatus(testContext1Stack).Return(types.StackStatusCreateComplete, nil)
 	errorMessage := "cannot upload input"
+	expectedInfix := "unable to sync s3://TestOutputBucket/project/TestProject1/userid/bender123/data/Workflow.variable/data.txt: "
 	s.mockProjectClient.EXPECT().Read().Return(s.testProjSpec, nil)
 	s.mockProjectClient.EXPECT().GetLocation().Return(testProjectFileDir)
 	s.mockSsmClient.EXPECT().GetOutputBucket().Return(testOutputBucket, nil)
@@ -426,7 +428,7 @@ func (s *WorkflowRunTestSuite) TestRunWorkflow_UploadInputFailed() {
 
 	actualId, err := s.manager.RunWorkflow(testContext1Name, testLocalWorkflowName, testArgumentsPath)
 	if s.Assert().Error(err) {
-		s.Assert().EqualError(err, testErrorPrefix+errorMessage)
+		s.Assert().EqualError(err, testErrorPrefix+expectedInfix+errorMessage)
 		s.Assert().Empty(actualId)
 	}
 }
