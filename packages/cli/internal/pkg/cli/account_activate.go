f0560267fbbffaa17f72a0764950ec86d005b63d
Cloudy Lopez <52179349+OhCloud@users.noreply.github.com>
2022-03-10 09:49:59 -0800

    feat: ensures vpcId is recalled (if available) on account_activate (#365)
    * account_activate recalls vpcId and unit testing completed
    
    * resolved comments including deleting log line, adding RenderCoreStackName(), adding back constants, and updating an error.
    
    * resolved comments including deleting log line, adding RenderCoreStackName(), adding back constants, updating an import, and updating an error.
    
    * updates the doc to reflect the updated vpcId recall behavior
    
    * fixed spacing
    
    * updating log line from Info to Debug

--- packages/cli/internal/pkg/cli/account_activate.go
+++ packages/cli/internal/pkg/cli/account_activate.go
@@ -5,14 +5,18 @@ package cli
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/cdk"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/cfn"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/ecr"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/s3"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/sts"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/awsresources"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/clierror"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/constants"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/logging"
@@ -51,6 +55,7 @@ type accountActivateOpts struct {
 	s3Client  s3.Interface
 	cdkClient cdk.Interface
 	ecrClient ecr.Interface
+	cfnClient cfn.Interface
 	imageRefs map[string]ecr.ImageReference
 	region    string
 }
@@ -61,40 +66,17 @@ func newAccountActivateOpts(vars accountActivateVars) (*accountActivateOpts, err
 		stsClient:           aws.StsClient(profile),
 		s3Client:            aws.S3Client(profile),
 		cdkClient:           cdk.NewClient(profile),
+		cfnClient:           aws.CfnClient(profile),
 		region:              aws.Region(profile),
 	}, nil
 }
 
 // Execute activates AGC.
 func (o *accountActivateOpts) Execute() error {
-	if o.bucketName == "" {
-		bucketName, err := o.generateDefaultBucket()
-		if err != nil {
-			return err
-		}
-		o.bucketName = bucketName
-	}
 
-	exists, err := o.s3Client.BucketExists(o.bucketName)
+	environmentVars, err := o.generateEnvVars()
 	if err != nil {
 		return err
-	}
-
-	environmentVars := []string{
-		fmt.Sprintf("%s=%s", constants.AgcBucketNameEnvKey, o.bucketName),
-		fmt.Sprintf("%s=%t", constants.CreateBucketEnvKey, !exists),
-		fmt.Sprintf("%s=%s", constants.AgcVersionEnvKey, version.Version),
-	}
-	if o.vpcId != "" {
-		environmentVars = append(environmentVars, fmt.Sprintf("%s=%s", constants.VpcIdEnvKey, o.vpcId))
-	}
-
-	if o.customTags != nil {
-		jsonBytes, err := json.Marshal(o.customTags)
-		if err != nil {
-			return err
-		}
-		environmentVars = append(environmentVars, fmt.Sprintf("%s=%s", constants.CustomTagEnvKey, string(jsonBytes)))
 	}
 
 	homeDir, err := osutils.DetermineHomeDir()
@@ -117,6 +99,65 @@ func (o accountActivateOpts) generateDefaultBucket() (string, error) {
 		return "", err
 	}
 	return generateBucketName(account, o.region), nil
+}
+
+func (o accountActivateOpts) generateEnvVars() ([]string, error) {
+	if o.bucketName == "" {
+		bucketName, err := o.generateDefaultBucket()
+		if err != nil {
+			return nil, err
+		}
+		o.bucketName = bucketName
+	}
+
+	exists, err := o.s3Client.BucketExists(o.bucketName)
+	if err != nil {
+		return nil, err
+	}
+
+	environmentVars := []string{
+		fmt.Sprintf("%s=%s", constants.AgcBucketNameEnvKey, o.bucketName),
+		fmt.Sprintf("%s=%t", constants.CreateBucketEnvKey, !exists),
+		fmt.Sprintf("%s=%s", constants.AgcVersionEnvKey, version.Version),
+	}
+
+	if o.customTags != nil {
+		jsonBytes, err := json.Marshal(o.customTags)
+		if err != nil {
+			return nil, err
+		}
+		environmentVars = append(environmentVars, fmt.Sprintf("%s=%s", constants.CustomTagEnvKey, string(jsonBytes)))
+	}
+
+	vpcId, err := o.getVpcId()
+	if err != nil {
+		return nil, err
+	}
+	if vpcId != "" {
+		environmentVars = append(environmentVars, fmt.Sprintf("VPC_ID=%s", vpcId))
+	}
+	return environmentVars, err
+}
+
+func (o accountActivateOpts) getVpcId() (string, error) {
+	if o.vpcId != "" {
+		return o.vpcId, nil
+	} else {
+		coreStackName := awsresources.RenderCoreStackName()
+		stackOutputs, err := o.cfnClient.GetStackOutputs(coreStackName)
+		if err != nil {
+			if errors.Is(err, cfn.StackDoesNotExistError) {
+				log.Debug().Msgf("Cloudformation Stack '%s' does not exist", coreStackName)
+				return "", nil
+			} else {
+				return "", err
+			}
+		}
+		if vpcId, ok := stackOutputs["VpcId"]; ok {
+			return vpcId, nil
+		}
+	}
+	return "", nil
 }
 
 func (o accountActivateOpts) cdkBootstrap(cdkAppPath string, environmentVars []string) error {
@@ -147,6 +188,7 @@ func displayProgress(progressStream cdk.ProgressStream, displayMsg string) error
 			}
 			lastEvent = event
 		}
+		log.Debug().Msg(strings.Join(lastEvent.Outputs, "\n"))
 	} else {
 		return progressStream.DisplayProgress(displayMsg)
 	}
