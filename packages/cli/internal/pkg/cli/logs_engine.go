97a0a05829e16bb2315f9a7f9578f8424477f575
Mark Schreiber <mrschre@amazon.com>
2022-03-18 18:55:19 -0400

    added run-id flag for logs engine

--- packages/cli/internal/pkg/cli/logs_engine.go
+++ packages/cli/internal/pkg/cli/logs_engine.go
@@ -4,15 +4,26 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/clierror"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/clierror/actionableerror"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/context"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
+const (
+	runIdFlag        = "run-id"
+	runIdShort       = "r"
+	runIdDescription = "filter to engine logs to this workflow run id"
+	runIdDefault     = ""
+)
+
 type logsEngineVars struct {
 	logsSharedVars
+	workflowRunId string
 }
 
 type logsEngineOpts struct {
@@ -33,6 +44,21 @@ func newLogsEngineOpts(vars logsEngineVars) (*logsEngineOpts, error) {
 func (o *logsEngineOpts) Validate() error {
 	if err := o.validateFlags(); err != nil {
 		return err
+	}
+
+	if o.workflowRunId == "" {
+		ctxMap, err := o.ctxManager.List()
+		if err != nil {
+			return err
+		}
+
+		summary := ctxMap[o.contextName]
+		for _, engine := range summary.Engines {
+			if engine.Engine != "cromwell" {
+				return actionableerror.New(fmt.Errorf("a workflow run must be specified if workflow engine is '%s'", engine.Engine),
+					"please run the command again with -r <run-id>")
+			}
+		}
 	}
 
 	return o.parseTime(o.logsSharedVars)
@@ -57,12 +83,12 @@ func (o *logsEngineOpts) Execute() error {
 func BuildLogsEngineCommand() *cobra.Command {
 	vars := logsEngineVars{}
 	cmd := &cobra.Command{
-		Use:   "engine -c context_name [-f filter] [-s start_date] [-e end_date] [-l look_back] [-t]",
+		Use:   "engine -c context_name [-r run_id] [-f filter] [-s start_date] [-e end_date] [-l look_back] [-t]",
 		Short: "Show workflow engine logs for a given context.",
 		Long: `Show workflow engine logs for a given context.
 If no start, end, or look back periods are set, this command will show logs from the last hour.`,
 		Example: `
-/code agc logs engine -c myCtx -s 2021/3/31 -e 2021/4/1 -f ERROR`,
+/code agc logs engine -c myCtx -r 1234-aed-32db -s 2021/3/31 -e 2021/4/1 -f ERROR`,
 		RunE: runCmdE(func(cmd *cobra.Command, args []string) error {
 			opts, err := newLogsEngineOpts(vars)
 			if err != nil {
@@ -71,8 +97,15 @@ If no start, end, or look back periods are set, this command will show logs from
 			if err = opts.Validate(); err != nil {
 				return err
 			}
-			opts.setDefaultEndTimeIfEmpty()
-			log.Info().Msgf("Showing engine logs for '%s'", opts.contextName)
+
+			if vars.workflowRunId == "" {
+				opts.setDefaultEndTimeIfEmpty()
+			}
+			var msg = fmt.Sprintf("Showing engine logs for '%s'", opts.contextName)
+			if vars.endString != "" {
+				msg = fmt.Sprintf("Showing engine logs for '%s' from '%s", opts.contextName, opts.endString)
+			}
+			log.Info().Msg(msg)
 			if err = opts.Execute(); err != nil {
 				return clierror.New("logs engine", vars, err)
 			}
@@ -81,5 +114,6 @@ If no start, end, or look back periods are set, this command will show logs from
 	}
 	vars.setFilterFlags(cmd)
 	vars.setContextFlag(cmd)
+	cmd.Flags().StringVarP(&vars.workflowRunId, runIdFlag, runIdShort, runIdDefault, runIdDescription)
 	return cmd
 }
--- packages/cli/internal/pkg/cli/logs_engine_test.go
+++ packages/cli/internal/pkg/cli/logs_engine_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/cwl"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/context"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/spec"
 	awsmocks "github.com/aws/amazon-genomics-cli/internal/pkg/mocks/aws"
 	contextmocks "github.com/aws/amazon-genomics-cli/internal/pkg/mocks/context"
 	"github.com/golang/mock/gomock"
@@ -19,7 +20,7 @@ const (
 )
 
 func TestLogsEngineOpts_Validate_LookBackFlag(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{lookBack: "5h"}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{lookBack: "5h"}, workflowRunId: "1234"}}
 	before := time.Now().Add(-5 * time.Hour)
 	err := opts.Validate()
 	after := time.Now().Add(-5 * time.Hour)
@@ -29,7 +30,7 @@ func TestLogsEngineOpts_Validate_LookBackFlag(t *testing.T) {
 }
 
 func TestLogsEngineOpts_Validate_LookBackError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{lookBack: "abc"}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{lookBack: "abc"}, workflowRunId: "1234"}}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("time: invalid duration \"abc\""), err)
 }
@@ -37,7 +38,7 @@ func TestLogsEngineOpts_Validate_LookBackError(t *testing.T) {
 func TestLogsEngineOpts_Validate_StartEndFlags(t *testing.T) {
 	start := time.Unix(0, 773391600000)
 	end := time.Unix(0, 773391700000)
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{startString: start.Format(time.RFC3339Nano), endString: end.Format(time.RFC3339Nano)}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{startString: start.Format(time.RFC3339Nano), endString: end.Format(time.RFC3339Nano)}, workflowRunId: "1234"}}
 	err := opts.Validate()
 	assert.NoError(t, err)
 	assert.True(t, start.Equal(*opts.startTime))
@@ -45,21 +46,45 @@ func TestLogsEngineOpts_Validate_StartEndFlags(t *testing.T) {
 }
 
 func TestLogsEngineOpts_Validate_StartError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{startString: "abc"}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{startString: "abc"}, workflowRunId: "1234"}}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Could not find format for \"abc\""), err)
 }
 
 func TestLogsEngineOpts_Validate_EndError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{endString: "abc"}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{endString: "abc"}, workflowRunId: "1234"}}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Could not find format for \"abc\""), err)
 }
 
 func TestLogsEngineOpts_Validate_FlagConflictError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{startString: "1/1/1990", lookBack: "1h"}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{startString: "1/1/1990", lookBack: "1h"}}}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("a look back period cannot be specified together with start or end times"), err)
+}
+
+func TestLogsEngineOpts_Validate_RunIdNotRequiredForCromwell(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: "myCtx"}}}
+	opts.ctxManager = ctxMock
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{"myCtx": {Engines: []spec.Engine{{Engine: "cromwell"}}}}, nil)
+
+	err := opts.Validate()
+	assert.NoError(t, err)
+}
+
+func TestLogsEngineOpts_Validate_RunIdRequiredForOtherEngines(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: "myCtx"}}}
+	opts.ctxManager = ctxMock
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{"myCtx": {Engines: []spec.Engine{{Engine: "other"}}}}, nil)
+
+	err := opts.Validate()
+	assert.EqualError(t, err, "an error occurred caused by: a workflow run must be specified if workflow engine is 'other'\nsuggestion: please run the command again with -r <run-id>\n")
 }
 
 func TestLogsEngineOpts_Execute_Group(t *testing.T) {
@@ -70,7 +95,7 @@ func TestLogsEngineOpts_Execute_Group(t *testing.T) {
 	logPaginatorMock := awsmocks.NewMockCwlLogPaginator(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
 	}
 
 	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{EngineLogGroupName: testLogGroupName}, nil)
@@ -89,7 +114,7 @@ func TestLogsEngineOpts_Execute_InfoError(t *testing.T) {
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
 	}
 
 	someErr := fmt.Errorf("some info error")
@@ -107,7 +132,7 @@ func TestLogsEngineOpts_Execute_LogError(t *testing.T) {
 	logPaginatorMock := awsmocks.NewMockCwlLogPaginator(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
 	}
 
 	someErr := fmt.Errorf("some log error")
@@ -127,7 +152,7 @@ func TestLogsEngineOpts_Execute_Stream(t *testing.T) {
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1, tail: true}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1, tail: true}},
 	}
 	stream := make(chan cwl.StreamEvent)
 	go func() { stream <- cwl.StreamEvent{Logs: []string{"log"}}; close(stream) }()
@@ -145,7 +170,7 @@ func TestLogsEngineOpts_Execute_StreamError(t *testing.T) {
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1, tail: true}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1, tail: true}},
 	}
 	someErr := fmt.Errorf("some stream error")
 	stream := make(chan cwl.StreamEvent)
