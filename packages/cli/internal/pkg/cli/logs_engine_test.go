8bda67492b3cf143ffd5a0a8931bbcdf3e04992d
Mark Schreiber <mrschre@amazon.com>
2022-03-28 14:32:08 -0400

    feat: Engine logs for specific workflow runs (#379)
    * added run-id flag for logs engine
    
    * Initial implementation of run-id filtering for workflow engine logs
    
    * Added unit tests for GetEngineLogByRunId
    
    * Handle wes adapter time edge case and cli log stream processing race condition where the engine process is running but the log stream doesn't exist yet.
    PR feedback
    
    * Switch to isServerProcessEngine as most engines are batch job processes so we only need to track the exceptions
    
    * allow output of both STDOUT and STDERR from engines

--- packages/cli/internal/pkg/cli/logs_engine_test.go
+++ packages/cli/internal/pkg/cli/logs_engine_test.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/cwl"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/context"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/spec"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/workflow"
 	awsmocks "github.com/aws/amazon-genomics-cli/internal/pkg/mocks/aws"
 	contextmocks "github.com/aws/amazon-genomics-cli/internal/pkg/mocks/context"
 	"github.com/golang/mock/gomock"
@@ -19,7 +21,14 @@ const (
 )
 
 func TestLogsEngineOpts_Validate_LookBackFlag(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{lookBack: "5h"}}}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	contextManager := buildMockCtxMgr(ctrl)
+
+	opts := logsEngineOpts{
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName, lookBack: "5h"}, workflowRunId: "1234"},
+		logsSharedOpts: logsSharedOpts{ctxManager: contextManager},
+	}
 	before := time.Now().Add(-5 * time.Hour)
 	err := opts.Validate()
 	after := time.Now().Add(-5 * time.Hour)
@@ -29,15 +38,27 @@ func TestLogsEngineOpts_Validate_LookBackFlag(t *testing.T) {
 }
 
 func TestLogsEngineOpts_Validate_LookBackError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{lookBack: "abc"}}}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	contextManager := buildMockCtxMgr(ctrl)
+	opts := logsEngineOpts{
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName, lookBack: "abc"}, workflowRunId: "1234"},
+		logsSharedOpts: logsSharedOpts{ctxManager: contextManager},
+	}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("time: invalid duration \"abc\""), err)
 }
 
 func TestLogsEngineOpts_Validate_StartEndFlags(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	contextManager := buildMockCtxMgr(ctrl)
 	start := time.Unix(0, 773391600000)
 	end := time.Unix(0, 773391700000)
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{startString: start.Format(time.RFC3339Nano), endString: end.Format(time.RFC3339Nano)}}}
+	opts := logsEngineOpts{
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName, startString: start.Format(time.RFC3339Nano), endString: end.Format(time.RFC3339Nano)}, workflowRunId: "1234"},
+		logsSharedOpts: logsSharedOpts{ctxManager: contextManager},
+	}
 	err := opts.Validate()
 	assert.NoError(t, err)
 	assert.True(t, start.Equal(*opts.startTime))
@@ -45,21 +66,45 @@ func TestLogsEngineOpts_Validate_StartEndFlags(t *testing.T) {
 }
 
 func TestLogsEngineOpts_Validate_StartError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{startString: "abc"}}}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	contextManager := buildMockCtxMgr(ctrl)
+	opts := logsEngineOpts{
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName, startString: "abc"}, workflowRunId: "1234"},
+		logsSharedOpts: logsSharedOpts{ctxManager: contextManager},
+	}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Could not find format for \"abc\""), err)
 }
 
 func TestLogsEngineOpts_Validate_EndError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{endString: "abc"}}}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	contextManager := buildMockCtxMgr(ctrl)
+	opts := logsEngineOpts{
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName, endString: "abc"}, workflowRunId: "1234"},
+		logsSharedOpts: logsSharedOpts{ctxManager: contextManager},
+	}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Could not find format for \"abc\""), err)
 }
 
 func TestLogsEngineOpts_Validate_FlagConflictError(t *testing.T) {
-	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars{startString: "1/1/1990", lookBack: "1h"}}}
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{startString: "1/1/1990", lookBack: "1h"}}}
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("a look back period cannot be specified together with start or end times"), err)
+}
+
+func TestLogsEngineOpts_Validate_RunIdNotRequiredForCromwell(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
+	opts := logsEngineOpts{logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: "myCtx"}}}
+	opts.ctxManager = ctxMock
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{"myCtx": {Engines: []spec.Engine{{Engine: "cromwell"}}}}, nil)
+
+	err := opts.Validate()
+	assert.NoError(t, err)
 }
 
 func TestLogsEngineOpts_Execute_Group(t *testing.T) {
@@ -70,7 +115,7 @@ func TestLogsEngineOpts_Execute_Group(t *testing.T) {
 	logPaginatorMock := awsmocks.NewMockCwlLogPaginator(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
 	}
 
 	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{EngineLogGroupName: testLogGroupName}, nil)
@@ -89,7 +134,7 @@ func TestLogsEngineOpts_Execute_InfoError(t *testing.T) {
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
 	}
 
 	someErr := fmt.Errorf("some info error")
@@ -107,7 +152,7 @@ func TestLogsEngineOpts_Execute_LogError(t *testing.T) {
 	logPaginatorMock := awsmocks.NewMockCwlLogPaginator(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
 	}
 
 	someErr := fmt.Errorf("some log error")
@@ -127,7 +172,7 @@ func TestLogsEngineOpts_Execute_Stream(t *testing.T) {
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1, tail: true}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1, tail: true}},
 	}
 	stream := make(chan cwl.StreamEvent)
 	go func() { stream <- cwl.StreamEvent{Logs: []string{"log"}}; close(stream) }()
@@ -145,7 +190,7 @@ func TestLogsEngineOpts_Execute_StreamError(t *testing.T) {
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsEngineOpts{
 		logsSharedOpts: logsSharedOpts{cwlClient: cwlMock, ctxManager: ctxMock},
-		logsEngineVars: logsEngineVars{logsSharedVars{contextName: testContextName1, tail: true}},
+		logsEngineVars: logsEngineVars{logsSharedVars: logsSharedVars{contextName: testContextName1, tail: true}},
 	}
 	someErr := fmt.Errorf("some stream error")
 	stream := make(chan cwl.StreamEvent)
@@ -155,4 +200,50 @@ func TestLogsEngineOpts_Execute_StreamError(t *testing.T) {
 
 	err := opts.Execute()
 	assert.Equal(t, someErr, err)
+}
+
+func Test_streamNamesFromRunLog(t *testing.T) {
+	const StdOutStream = "stdout/log/stream"
+	const StdErrStream = "stderr/log/stream"
+
+	type test struct {
+		workflowRunLog workflow.EngineLog
+		expect         []string
+	}
+
+	tests := map[string]test{
+		"empty STDOUT and STDERR produce empty array": {
+			workflowRunLog: workflow.EngineLog{},
+			expect:         []string{}},
+		"STDOUT stream name is present": {
+			workflowRunLog: workflow.EngineLog{StdOut: StdOutStream},
+			expect:         []string{StdOutStream},
+		},
+		"STDERR stream name is present": {
+			workflowRunLog: workflow.EngineLog{StdErr: StdErrStream},
+			expect:         []string{StdErrStream},
+		},
+		"STDOUT and STDERR stream names are present": {
+			workflowRunLog: workflow.EngineLog{StdOut: StdOutStream, StdErr: StdErrStream},
+			expect:         []string{StdOutStream, StdErrStream},
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			actual := streamNamesFromRunLog(tt.workflowRunLog)
+			assert.Equal(t, tt.expect, actual)
+		})
+	}
+}
+
+func buildMockCtxMgr(ctrl *gomock.Controller) *contextmocks.MockContextManager {
+	contextManager := contextmocks.NewMockContextManager(ctrl)
+	contextManager.EXPECT().List().Return(
+		map[string]context.Summary{
+			testContextName: {
+				Engines: []spec.Engine{{Type: "foo", Engine: "foo"}},
+			},
+		}, nil)
+	return contextManager
 }
---
+++ packages/cli/internal/pkg/cli/workflow/workflow_engine.go
@@ -0,0 +1,39 @@
+package workflow
+
+import (
+	"github.com/rsc/wes_client"
+)
+
+type EngineLog struct {
+	WorkflowRunId  string
+	StdOut         string
+	StdErr         string
+	WorkflowStatus wes_client.State
+}
+
+func (m *Manager) GetEngineLogByRunId(runId string) (EngineLog, error) {
+	m.readProjectSpec()
+	m.readConfig()
+	m.setContextForRun(runId)
+	m.setContext(m.runContextName)
+	m.setEngineForWorkflowType(m.runContextName)
+	m.setContextStackInfo(m.runContextName)
+	m.setWesUrl()
+	m.setWesClient()
+	m.getRunLog(runId)
+
+	return m.buildEngineLog()
+}
+
+func (m *Manager) buildEngineLog() (EngineLog, error) {
+	if m.err != nil {
+		return EngineLog{}, m.err
+	}
+
+	return EngineLog{
+		WorkflowRunId:  m.taskProps.runLog.RunId,
+		StdOut:         m.taskProps.runLog.RunLog.Stdout,
+		StdErr:         m.taskProps.runLog.RunLog.Stderr,
+		WorkflowStatus: m.taskProps.runLog.State,
+	}, nil
+}
