ba3ec55c3a011e478645fcab80a5874d93a3586e
Adam Novak <anovak@soe.ucsc.edu>
2022-05-06 15:36:42 -0700

    Allow but do not require contexts to be retrieved

--- packages/cli/internal/pkg/cli/logs_adapter_test.go
+++ packages/cli/internal/pkg/cli/logs_adapter_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/amazon-genomics-cli/internal/pkg/aws/cwl"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/context"
 	"github.com/aws/amazon-genomics-cli/internal/pkg/cli/spec"
-    "github.com/aws/amazon-genomics-cli/internal/pkg/constants"
+	"github.com/aws/amazon-genomics-cli/internal/pkg/constants"
 	awsmocks "github.com/aws/amazon-genomics-cli/internal/pkg/mocks/aws"
 	contextmocks "github.com/aws/amazon-genomics-cli/internal/pkg/mocks/context"
 	"github.com/golang/mock/gomock"
@@ -17,14 +17,14 @@ import (
 )
 
 func TestLogsAdapterOpts_Validate_LookBackFlag(t *testing.T) {
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-    ctxMock := contextmocks.NewMockContextManager(ctrl)
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, lookBack: "5h"}},
-    }
-    ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil)
+		logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, lookBack: "5h"}},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	before := time.Now().Add(-5 * time.Hour)
 	err := opts.Validate()
 	after := time.Now().Add(-5 * time.Hour)
@@ -34,14 +34,14 @@ func TestLogsAdapterOpts_Validate_LookBackFlag(t *testing.T) {
 }
 
 func TestLogsAdapterOpts_Validate_LookBackError(t *testing.T) {
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-    ctxMock := contextmocks.NewMockContextManager(ctrl)
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, lookBack: "abc"}},
-    }
-    ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil)
+		logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, lookBack: "abc"}},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("time: invalid duration \"abc\""), err)
 }
@@ -49,20 +49,18 @@ func TestLogsAdapterOpts_Validate_LookBackError(t *testing.T) {
 func TestLogsAdapterOpts_Validate_StartEndFlags(t *testing.T) {
 	start := time.Unix(0, 773391600000)
 	end := time.Unix(0, 773391700000)
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-    ctxMock := contextmocks.NewMockContextManager(ctrl)
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{
-            logsSharedVars{
-                contextName: testContextName1,
-                startString: start.Format(time.RFC3339Nano),
-                endString: end.Format(time.RFC3339Nano),
-            },
-        },
-    }
-    ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil)
+		logsSharedOpts: logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{
+			logsSharedVars{
+				contextName: testContextName1, startString: start.Format(time.RFC3339Nano), endString: end.Format(time.RFC3339Nano),
+			},
+		},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	err := opts.Validate()
 	assert.NoError(t, err)
 	assert.True(t, start.Equal(*opts.startTime))
@@ -70,39 +68,40 @@ func TestLogsAdapterOpts_Validate_StartEndFlags(t *testing.T) {
 }
 
 func TestLogsAdapterOpts_Validate_StartError(t *testing.T) {
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-    ctxMock := contextmocks.NewMockContextManager(ctrl)
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, startString: "abc"}},
-    }
-    ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil)
+		logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, startString: "abc"}},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Could not find format for \"abc\""), err)
 }
 
 func TestLogsAdapterOpts_Validate_EndError(t *testing.T) {
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-    ctxMock := contextmocks.NewMockContextManager(ctrl)
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, endString: "abc"}},
-    }
-    ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil)
+		logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, endString: "abc"}},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Could not find format for \"abc\""), err)
 }
 
 func TestLogsAdapterOpts_Validate_FlagConflictError(t *testing.T) {
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-    ctxMock := contextmocks.NewMockContextManager(ctrl)
+	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, startString: "1/1/1990", lookBack: "1h"}},
-    }
+		logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1, startString: "1/1/1990", lookBack: "1h"}},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("a look back period cannot be specified together with start or end times"), err)
 }
@@ -112,10 +111,10 @@ func TestLogsAdapterOpts_Validate_ToilError(t *testing.T) {
 	defer ctrl.Finish()
 	ctxMock := contextmocks.NewMockContextManager(ctrl)
 	opts := logsAdapterOpts{
-        logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
-        logsAdapterVars: logsAdapterVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
-    }
-	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.TOIL}}}}, nil)
+		logsSharedOpts:  logsSharedOpts{ctxManager: ctxMock},
+		logsAdapterVars: logsAdapterVars{logsSharedVars: logsSharedVars{contextName: testContextName1}},
+	}
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.TOIL}}}}, nil).AnyTimes()
 
 	err := opts.Validate()
 	assert.Equal(t, fmt.Errorf("Contexts using the toil engine do not have adapters to collect logs from"), err)
@@ -132,6 +131,7 @@ func TestLogsAdapterOpts_Execute_Group(t *testing.T) {
 		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1}},
 	}
 
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{WesLogGroupName: testLogGroupName}, nil)
 	cwlMock.EXPECT().GetLogsPaginated(cwl.GetLogsInput{LogGroupName: testLogGroupName}).Return(logPaginatorMock)
 	gomock.InOrder(logPaginatorMock.EXPECT().HasMoreLogs().Return(true), logPaginatorMock.EXPECT().HasMoreLogs().Return(false))
@@ -151,7 +151,8 @@ func TestLogsAdapterOpts_Execute_InfoError(t *testing.T) {
 		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1}},
 	}
 
-    someErr := fmt.Errorf("some info error")
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
+	someErr := fmt.Errorf("some info error")
 	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{}, someErr)
 
 	err := opts.Execute()
@@ -169,6 +170,7 @@ func TestLogsAdapterOpts_Execute_LogError(t *testing.T) {
 		logsAdapterVars: logsAdapterVars{logsSharedVars{contextName: testContextName1}},
 	}
 
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
 	someErr := fmt.Errorf("some log error")
 	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{WesLogGroupName: testLogGroupName}, nil)
 	cwlMock.EXPECT().GetLogsPaginated(cwl.GetLogsInput{LogGroupName: testLogGroupName}).Return(logPaginatorMock)
@@ -190,7 +192,8 @@ func TestLogsAdapterOpts_Execute_Stream(t *testing.T) {
 	}
 	stream := make(chan cwl.StreamEvent)
 	go func() { stream <- cwl.StreamEvent{Logs: []string{"log"}}; close(stream) }()
-    ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{WesLogGroupName: testLogGroupName}, nil)
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
+	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{WesLogGroupName: testLogGroupName}, nil)
 	cwlMock.EXPECT().StreamLogs(ctx.Background(), testLogGroupName).Return(stream)
 
 	err := opts.Execute()
@@ -209,7 +212,8 @@ func TestLogsAdapterOpts_Execute_StreamError(t *testing.T) {
 	someErr := fmt.Errorf("some stream error")
 	stream := make(chan cwl.StreamEvent)
 	go func() { stream <- cwl.StreamEvent{Err: someErr} }()
-    ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{WesLogGroupName: testLogGroupName}, nil)
+	ctxMock.EXPECT().List().Return(map[string]context.Summary{testContextName1: {Engines: []spec.Engine{{Engine: constants.CROMWELL}}}}, nil).AnyTimes()
+	ctxMock.EXPECT().Info(testContextName1).Return(context.Detail{WesLogGroupName: testLogGroupName}, nil)
 	cwlMock.EXPECT().StreamLogs(ctx.Background(), testLogGroupName).Return(stream)
 
 	err := opts.Execute()
