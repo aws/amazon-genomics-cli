09700916b93319c4dc3435bef18ba3b4c7f8e4d5
Mark Schreiber <mrschre@amazon.com>
2022-06-08 13:05:00 -0400

    added debug (verbose) statements to workflow run logic.

--- packages/cli/internal/pkg/cli/workflow/workflow_run.go
+++ packages/cli/internal/pkg/cli/workflow/workflow_run.go
@@ -2,7 +2,7 @@ package workflow
 
 import "fmt"
 
-func (m *Manager) RunWorkflow(contextName, workflowName, argumentsUrl string, optionFileUrl string) (string, error) {
+func (m *Manager) RunWorkflow(contextName, workflowName, inputsFileUrl string, optionFileUrl string) (string, error) {
 	m.readProjectSpec()
 	m.setWorkflowSpec(workflowName)
 	m.readConfig()
@@ -19,7 +19,7 @@ func (m *Manager) RunWorkflow(contextName, workflowName, argumentsUrl string, op
 		m.cleanUpWorkflow()
 	}
 	m.calculateFinalLocation()
-	m.readInput(argumentsUrl)
+	m.readInput(inputsFileUrl)
 	m.uploadInputsToS3()
 	m.parseInputToArguments()
 	m.readOptionFile(optionFileUrl)
