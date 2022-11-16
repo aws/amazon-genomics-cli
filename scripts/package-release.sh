ca088266ffd5c85f4362a6b7fb9a219bf18b7345
Michael Christenson II <michael@xorsense.nl>
2022-11-16 12:07:11 -0500

    feat: Add ARM support for CLI build
    Summary
    =======
    
    This commit focuses on two related `agc` CLI build and release issues:
    
    - Apple ARM support in V8
    - ARM build and release support
    
    All of these issues are focused on developing the CLI and nothing else. Details follow below.
    
    Apple ARM support in V8
    =======================
    
    The current version of NodeJS set in `.nvmrc` has a version of V8 that does not support Apple's ARM family of processors. This caused issues where following the [README](https://github.com/aws/amazon-genomics-cli#readme) for building the CLI, failed at the [NVM step](https://github.com/aws/amazon-genomics-cli#node). Support for Apple ARM was back ported to NodeJs v14 LTS in 14.17.0.
    
    Additionaly, NodeJs compilation would fail if you had Python 1.10 or greater installed on your Mac.
    
    Fixes
    -----
    
    - Uses newer V8 support for Python 3.10+
    - Uses newer V8 support of Apple ARM processor family
    - Includes various NodeJS v14 LTS bug and security fixes
    
    ARM build and release support
    =============================
    
    The current Make compile targets made assumptions about what the Go architecture and output would be; these were amd64 centric.
    
    As well, there was a subtle bug in the release packaging that assumed jq, as a dependency, was installed; which may not be true by default.
    
    Fixes
    -----
    
    - Add ARM compile targets for Darwin and Linux
    - Include ARM compile targets in release packaging
    - Exit release packing early if missing dependency jq

--- scripts/package-release.sh
+++ scripts/package-release.sh
@@ -3,6 +3,12 @@
 set -eo pipefail
 
 RELEASE_DIR="dist/amazon-genomics-cli"
+JQ=$(which jq)
+
+if [[ -z "$JQ" ]]; then
+  echo "Missing required jq package"
+  exit 1
+fi
 
 mkdir -p ${RELEASE_DIR}
 cp ./{LICENSE,THIRD-PARTY,CHANGELOG.md} ${RELEASE_DIR}
@@ -13,7 +19,7 @@ cp -a scripts/cli/. ${RELEASE_DIR}
 cp -a examples ${RELEASE_DIR}
 cp -a extras ${RELEASE_DIR}
 cp -a packages/cli/bin/local/. ${RELEASE_DIR}
-version=$(jq .version -r < version.json)
+version=$(${JQ} .version -r < version.json)
 commit="${CODEBUILD_RESOLVED_SOURCE_VERSION:-$(git rev-parse --verify HEAD)}"
 
 cat > ${RELEASE_DIR}/build.json <<HERE
