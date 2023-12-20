14de12cd0fb633f950f4ea6f84afa5c9ce82a300
cloud303-tfurlong <tfurlong@cloud303.io>
2023-12-20 17:37:55 -0500

    updated requirements

--- packages/wes_adapter/install.sh
+++ packages/wes_adapter/install.sh
@@ -6,5 +6,5 @@ if ! hash python3.9; then
 fi
 
 rm -rf ./dist && mkdir ./dist
-python3.9 -m pip install -r requirements.txt --target ./dist && (cd ./dist && zip -r ./wes_adapter.zip .)
+python3 -m pip install -r requirements.txt --target ./dist && (cd ./dist && zip -r ./wes_adapter.zip .)
 zip -gr ./dist/wes_adapter.zip ./rest_api ./amazon_genomics ./index.py
